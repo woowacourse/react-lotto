ba0eb52193f8db8e1d3231a43802bb6fd5f6d8e9
zereight <fullstackmachinelearning@gmail.com>
2021-04-13 15:40:29 +0900

    feat: Header 추가

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
+import Header from "./Header";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 export default class App extends Component {
   render() {
-    return <></>;
+    return (
+      <Container>
+        <Header />
+      </Container>
+    );
   }
 }
