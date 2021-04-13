3c67d966e008f3cc86dfe80d3b6b9961e3f897cc
swon3210 <swon3210@gmail.com>
2021-04-13 15:25:21 +0900

    chore: 기본 컴포넌트 초기화

---
+++ src/components/PriceInput/PriceInput.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+// type PriceInputProps = {};
+
+export default class PriceInput extends Component {
+  render() {
+    return <div>PriceInput</div>;
+  }
+}
