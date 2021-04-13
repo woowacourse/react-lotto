86b0e7195bf2b8face573358857a90b349a7ca5b
swon3210 <swon3210@gmail.com>
2021-04-13 21:00:49 +0900

    feat: PriceInputForm, TicketList 컴포넌트 마크업 및 스타일링 구현

---
+++ src/common/Button.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: #47a2b5;
+  height: 36px;
+  min-width: 64px;
+  padding: 0 16px;
+  border-radius: 4px;
+  outline: 0;
+  border-style: none;
+  cursor: pointer;
+`;
+
+export default Button;
--- src/components/PriceInput/PriceInput.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-// type PriceInputProps = {};
-
-export default class PriceInput extends Component {
-  render() {
-    return <div>PriceInput</div>;
-  }
-}
