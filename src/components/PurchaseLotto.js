66b61801dc4ec2e6fb16ea1cfc2561374b7846db
365kim <365listener@gmail.com>
2021-04-13 22:04:34 +0900

    feat: 앱 전체 구조 설정
    Co-authored-by: JUNMO HAN <wnsah052@naver.com>

---
+++ src/components/PurchaseLotto.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class PurchaseLotto extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return <div> </div>;
+  }
+}
