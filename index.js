2d9cea2e7480b544a23fd7a1bc675d7b4356bd06
iborymagic <mlch8023@gmail.com>
2021-04-13 22:01:16 +0900

    chore: scss 환경 설정

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.scss';
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
@@ -9,20 +10,37 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  devServer: {
+    hot: true,
+    inline: true,
+    host: 'localhost',
+    port: 5500,
+    contentBase: path.join(__dirname, '/dist/'),
+  },
   module: {
     rules: [
       { test: /\.(js)$/, exclude: /node_modules/, use: 'babel-loader' },
-      { test: /\.(css)$/, use: ['css-loader', 'style-loader'] },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
   },
   mode: 'development',
   devtool: 'eval',
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx', 'scss'],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'app.css',
     }),
   ],
 };
