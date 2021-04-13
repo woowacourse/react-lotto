81d7ca4ad63eb9e9fc0cc933528d18df72456c44
Shim MunSeong <Puterism.K@gmail.com>
2021-04-13 18:22:43 +0900

    chore: Babel 설정

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,7 +4,19 @@ module.exports = {
     public: { url: '/', static: true },
     src: { url: '/dist' },
   },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    [
+      '@snowpack/plugin-babel',
+      {
+        input: ['.js', '.mjs', '.jsx', '.ts', '.tsx'], // (optional) specify files for Babel to transform
+        transformOptions: {
+          // babel transform options
+        },
+      },
+    ],
+  ],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
