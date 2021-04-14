a5586f82f6b7d8ab05c1f9b7800cff56a655b29e
Chayan <hchayan196@gmail.com>
2021-04-14 12:10:00 +0900

    refactor: PriceinputForm 네이밍 PaymentForm으로 변경

---
+++ src/components/PaymentForm/PaymentForm.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Button from '../../common/Button';
+import Input from '../../common/Input';
+import { PaymentFormWrapper, PaymentFormLabel, PaymentWrapper } from './PaymentForm.styles';
+
+export default class PaymentForm extends Component {
+  render() {
+    return (
+      <PaymentFormWrapper>
+        <PaymentFormLabel>구입할 금액을 입력해주세요</PaymentFormLabel>
+        <PaymentWrapper>
+          <Input type="number" placeholder="구입 금액" name="payment-input" required />
+          <Button>확인</Button>
+        </PaymentWrapper>
+      </PaymentFormWrapper>
+    );
+  }
+}
--- src/components/TicketList/TicketItem/TicketItem.styles.tsx
+++ src/components/TicketList/TicketItem/TicketItem.styles.tsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 
 export const TicketItemWrapper = styled.div`
-  display: inline-block;
+  display: flex;
+  align-items: center;
 
   .ticket-icon {
     margin: 0 10px;
-    font-size: 1rem;
+    font-size: 2.25rem;
+  }
+
+  .ticket-number {
+    line-height: 3rem;
   }
 `;
