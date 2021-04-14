5f91d2206e87f7ff2fa110da292f7bd829ac6083
sunhpark42 <sunhpark42@gmail.com>
2021-04-14 15:05:53 +0900

    fix: table에 tbody 태그 추가

--- src/components/Modal/ResultSection.js
+++ src/components/Modal/ResultSection.js
@@ -8,46 +8,48 @@ export default class ResultSection extends Component {
   render() {
     return (
       <table className="mt-3">
-        <tr>
-          <th className="p-2">일치 갯수</th>
-          <th className="p-2">당첨금</th>
-          <th className="p-2">당첨 갯수</th>
-        </tr>
-        <tr className="text-center">
-          <td className="p-2">3개</td>
-          <td className="p-2">5,000</td>
-          <td className="p-2">
-            <span>0</span>개
-          </td>
-        </tr>
-        <tr className="text-center">
-          <td className="p-2">4개</td>
-          <td className="p-2">50,000</td>
-          <td className="p-2">
-            <span>0</span>개
-          </td>
-        </tr>
-        <tr className="text-center">
-          <td className="p-2">5개</td>
-          <td className="p-2">1,500,000</td>
-          <td className="p-2">
-            <span>0</span>개
-          </td>
-        </tr>
-        <tr className="text-center">
-          <td className="p-2">5개 + 보너스볼</td>
-          <td className="p-2">30,000,000</td>
-          <td className="p-2">
-            <span>0</span>개
-          </td>
-        </tr>
-        <tr className="text-center">
-          <td className="p-2">6개</td>
-          <td className="p-2">2,000,000,000</td>
-          <td className="p-2">
-            <span>0</span>개
-          </td>
-        </tr>
+        <tbody>
+          <tr>
+            <th className="p-2">일치 갯수</th>
+            <th className="p-2">당첨금</th>
+            <th className="p-2">당첨 갯수</th>
+          </tr>
+          <tr className="text-center">
+            <td className="p-2">3개</td>
+            <td className="p-2">5,000</td>
+            <td className="p-2">
+              <span>0</span>개
+            </td>
+          </tr>
+          <tr className="text-center">
+            <td className="p-2">4개</td>
+            <td className="p-2">50,000</td>
+            <td className="p-2">
+              <span>0</span>개
+            </td>
+          </tr>
+          <tr className="text-center">
+            <td className="p-2">5개</td>
+            <td className="p-2">1,500,000</td>
+            <td className="p-2">
+              <span>0</span>개
+            </td>
+          </tr>
+          <tr className="text-center">
+            <td className="p-2">5개 + 보너스볼</td>
+            <td className="p-2">30,000,000</td>
+            <td className="p-2">
+              <span>0</span>개
+            </td>
+          </tr>
+          <tr className="text-center">
+            <td className="p-2">6개</td>
+            <td className="p-2">2,000,000,000</td>
+            <td className="p-2">
+              <span>0</span>개
+            </td>
+          </tr>
+        </tbody>
       </table>
     );
   }
