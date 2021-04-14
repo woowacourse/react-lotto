faa7a06089f0c1db68047c0616a94287801f8616
zigsong <wldms5764@gmail.com>
2021-04-14 14:22:08 +0900

    chore: styled-component 세팅
    Co-authored-by: 서지환 <seojihwan213@naver.com>

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['babel-plugin-styled-components'],
           },
         },
       },
