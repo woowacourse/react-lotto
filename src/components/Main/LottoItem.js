803ebbcd98ba36f878cfc31cbb93c41648092cb7
sunhpark42 <sunhpark42@gmail.com>
2021-04-14 15:40:45 +0900

    feat: 로또 번호 자동 생성 기능 추가

--- src/components/Main/LottoItem.js
+++ src/components/Main/LottoItem.js
@@ -1,15 +1,36 @@
 import { Component } from 'react';
+import { LOTTO_VALUE } from '../../constants';
 
 export default class LottoItem extends Component {
   constructor() {
     super();
+
+    this.state = {
+      numbers: this.generateLottoNumbers(),
+    };
+  }
+
+  getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  generateLottoNumbers() {
+    const numbers = new Set();
+
+    while (numbers.size < LOTTO_VALUE.NUMBER_COUNT) {
+      const number = this.getRandomNumber(LOTTO_VALUE.MIN_NUMBER, LOTTO_VALUE.MAX_NUMBER);
+
+      numbers.add(number);
+    }
+
+    return numbers;
   }
 
   render() {
     return (
       <li className="lotto-item">
         <span className="lotto-icon">🎟</span>
-        <span>1, 2, 3, 4, 5, 6</span>
+        <span>{[...this.state.numbers].join(', ')}</span>
       </li>
     );
   }
--- src/constants/index.js
+++ src/constants/index.js
@@ -19,6 +19,12 @@ export const ID = {
 
 export const LOTTO_PRICE = 1000;
 
+export const LOTTO_VALUE = {
+  NUMBER_COUNT: 6,
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 45,
+};
+
 export const MESSAGE = {
   ALERT: {
     INVALID_MONEY_UNIT: '1000원 단위로만 구매 가능합니다.',
