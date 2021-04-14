969d86e24f9e37e99b4569bb7beac19075589d3f
yujo <bedro27@gmail.com>
2021-04-14 14:45:15 +0900

    chore: react를 사용하기 위한 webpack 초기 세팅

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Hello React</div>
+  </React.StrictMode>,
+  document.querySelector("#root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env, argv) => {
+  return {
+    entry: "./src/index.js",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "build"),
+      clean: true,
+    },
+    devServer: {
+      port: 3000,
+    },
+    devtool: argv.mode === "development" ? "eval-source-map" : "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: "babel-loader",
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "public/index.html" }),
+      new MiniCssExtractPlugin({ filename: "style.css" }),
+    ],
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    performance: {
+      hints: "warning",
+    },
+  };
+};
