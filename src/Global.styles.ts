52a1c273790de9e46d90a89f061c7e146412b137
Chayan <hchayan196@gmail.com>
2021-04-14 12:10:00 +0900

    refactor: PriceinputForm 네이밍 PaymentForm으로 변경

--- src/Global.styles.ts
+++ src/Global.styles.ts
@@ -4,10 +4,12 @@ import { normalize } from 'styled-normalize';
 const GlobalStyle = createGlobalStyle`
   ${normalize}
   html,
-  body {
+  body,
+  ul {
     margin: 0;
     height: 100%;
     font-size: 16px;
+    padding: 0;
   }
 `;
 
---
+++ src/components/PaymentForm/PaymentForm.styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const PaymentFormWrapper = styled.form``;
+
+export const PaymentFormLabel = styled.label`
+  display: inline-block;
+  margin-bottom: 20px;
+`;
+
+export const PaymentWrapper = styled.div`
+  display: flex;
+`;
