97026def769902ed6923e3980f5b0cff316f6973
seojihwan <seojihwan213@naver.com>
2021-04-14 17:39:09 +0900

    feat: ResultModal UI 구현
    Co-authored-by: jieun song <zigsong@users.noreply.github.com>

---
+++ src/components/ResultModal/index.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import {
+  Root,
+  Modal,
+  ModalInner,
+  CloseButton,
+  CloseX,
+  Title,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  EarningRate,
+  RestartButton,
+} from './style';
+
+class ResultModal extends Component {
+  render() {
+    return (
+      <Root>
+        <Modal>
+          <ModalInner>
+            <CloseButton>
+              <svg viewBox="0 0 40 40">
+                <CloseX d="M 10,10 L 30,30 M 30,10 L 10,30" />
+              </svg>
+            </CloseButton>
+            <Title>💰당첨 통계💰</Title>
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th>일치 갯수</Th>
+                  <Th>당첨금</Th>
+                  <Th>당첨 갯수</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>3개</Td>
+                  <Td>5,000</Td>
+                  <Td>n개</Td>
+                </Tr>
+                <Tr>
+                  <Td>4개</Td>
+                  <Td>50,000</Td>
+                  <Td>n개</Td>
+                </Tr>
+                <Tr>
+                  <Td>5개</Td>
+                  <Td>1,500,000</Td>
+                  <Td>n개</Td>
+                </Tr>
+                <Tr>
+                  <Td>5개 + 보너스볼</Td>
+                  <Td>30,000,000</Td>
+                  <Td>n개</Td>
+                </Tr>
+                <Tr>
+                  <Td>6개</Td>
+                  <Td>2,000,000,000</Td>
+                  <Td>n개</Td>
+                </Tr>
+              </Tbody>
+            </Table>
+            <EarningRate>당신의 총 수익률은 %입니다.</EarningRate>
+            <RestartButton>다시 시작하기</RestartButton>
+          </ModalInner>
+        </Modal>
+      </Root>
+    );
+  }
+}
+
+export default ResultModal;
