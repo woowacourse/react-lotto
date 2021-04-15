7e61ec52a8c05817f014065cbdcabecf3755e2b4
Kyle <mkitigy@gmail.com>
2021-04-15 15:07:22 +0900

    feat: add toggle button

--- src/components/LotteriesDetail.jsx
+++ src/components/LotteriesDetail.jsx
@@ -1,15 +1,43 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class LotteriesDetail extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isNumberVisible: false,
+    };
+  }
+
+  handleNumberDisplay = () => {
+    this.setState({ isNumberVisible: !this.state.isNumberVisible });
+  };
+
   render() {
+    const { isNumberVisible } = this.state;
+    const { lotteries } = this.props;
+
     return (
-      <ul>
-        {this.props.lotteries.map((lottery) => (
-          <li key={lottery.id}>
-            {lottery.id} :{lottery.numbers.join(",")}
-          </li>
-        ))}
-      </ul>
+      <section>
+        <div className="d-flex">
+          <span>총 {lotteries.length}개를 구매하였습니다.</span>
+          <div className="flex-auto d-flex justify-end pr-1">
+            <label className="switch">
+              <input type="checkbox" onChange={this.handleNumberDisplay} />
+              <span className="text-base font-normal">번호보기</span>
+            </label>
+          </div>
+        </div>
+        <ul className="d-flex">
+          {lotteries.map(lottery => (
+            <li className="d-flex" key={lottery.id}>
+              <span>🎟️</span>
+              <span className={isNumberVisible ? '' : 'd-none'}>
+                {lottery.numbers.join(', ')}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </section>
     );
   }
 }
