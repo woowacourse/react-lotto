3f86ed758de3cec07e434830112962cc7d8c00ab
0imbean0 <tjsqls2003@gmail.com>
2021-04-15 14:46:04 +0900

    feat: 구매 금액 입력 기능

--- src/components/LottoGame/PurchaseAmountForm/index.js
+++ src/components/LottoGame/PurchaseAmountForm/index.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 export default class PurchaseAmountForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.input = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const purchaseAmount = this.input.current.value;
+    this.props.setPurchaseAmount(purchaseAmount);
+  }
+
   render() {
     return (
-      <form id="purchase-price-input-form" className="mt-5">
-        <label for="purchase-price-input-form__input" className="mb-2 d-inline-block">
+      <form id="purchase-price-input-form" className="mt-5" onSubmit={this.handleSubmit}>
+        <label htmlFor="purchase-price-input-form__input" className="mb-2 d-inline-block">
           구입할 금액을 입력해주세요.
         </label>
         <div className="flex">
@@ -14,9 +28,15 @@ export default class PurchaseAmountForm extends Component {
             type="number"
             className="w-full mr-2 pl-2"
             placeholder="구입 금액"
+            ref={this.input}
+            disabled={this.props.isPurchaseAmountSubmitted}
             required
           />
-          <button id="purchase-price-input-form__button" type="submit" className="btn btn-cyan">
+          <button
+            id="purchase-price-input-form__button"
+            className="btn btn-cyan"
+            disabled={this.props.isPurchaseAmountSubmitted}
+          >
             확인
           </button>
         </div>
--- src/components/LottoGame/index.js
+++ src/components/LottoGame/index.js
@@ -6,18 +6,42 @@ import LottoResultContainer from './LottoResultContainer';
 import Modal from '../Modal';
 
 export default class LottoGame extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      purchaseAmount: 0,
+      lottoTickets: [],
+      resultNumberList: [],
+      isModalOpened: false,
+    };
+
+    this.setPurchaseAmount = this.setPurchaseAmount.bind(this);
+  }
+
+  isPurchaseAmountSubmitted() {
+    return this.state.purchaseAmount !== 0;
+  }
+
+  setPurchaseAmount(purchaseAmount) {
+    this.setState({ purchaseAmount });
+  }
+
   render() {
     return (
       <>
         <div className="flex justify-center mt-5">
           <div className="w-full">
             <h1 className="text-center">🎱 행운의 로또</h1>
-            <PurchaseAmountForm />
-            <LottoTicketList />
-            <LottoResultForm />
+            <PurchaseAmountForm
+              setPurchaseAmount={this.setPurchaseAmount}
+              isPurchaseAmountSubmitted={this.isPurchaseAmountSubmitted()}
+            />
+            {this.isPurchaseAmountSubmitted() && <LottoTicketList />}
+            {this.isPurchaseAmountSubmitted() && <LottoResultForm />}
           </div>
         </div>
-        <Modal container={<LottoResultContainer />} />
+        {this.state.isModalOpened && <Modal container={<LottoResultContainer />} />}
       </>
     );
   }
