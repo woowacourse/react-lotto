f1b54a1678de94977089ddc3c00cc78b9821a56a
zigsong <wldms5764@gmail.com>
2021-04-15 15:30:52 +0900

    chore: core-js 세팅

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,16 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'usage',
+                  corejs: '3.10',
+                },
+              ],
+              '@babel/preset-react',
+            ],
             plugins: ['babel-plugin-styled-components'],
           },
         },
