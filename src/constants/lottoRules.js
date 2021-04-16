c91848223492645ea2fbdb96643ba6f7b3da4306
jum0 <wnsah052@naver.com>
2021-04-17 00:32:26 +0900

    feat: 구매한 복권별 당첨 여부 확인 기능 구현

--- src/constants/lottoRules.js
+++ src/constants/lottoRules.js
@@ -10,38 +10,37 @@ export const BONUS_NUMBER_LENGTH = 1;
 export const BONUS_CHECK_REQUIRED_COUNT = 5;
 export const BONUS_COUNT = 0.5;
 
-export const RESULT_TABLE_KEY_LIST = [6, 5.5, 5, 4, 3, 2, 1, 0];
 export const RESULT_TABLE_DATA = {
   0: {
-    DESCRIPTION: '0개',
+    DESCRIPTION: '낙첨',
     PRIZE: 0,
   },
   1: {
-    DESCRIPTION: '1개',
+    DESCRIPTION: '낙첨',
     PRIZE: 0,
   },
   2: {
-    DESCRIPTION: '2개',
+    DESCRIPTION: '낙첨',
     PRIZE: 0,
   },
   3: {
-    DESCRIPTION: '3개',
+    DESCRIPTION: '5등',
     PRIZE: 5000,
   },
   4: {
-    DESCRIPTION: '4개',
+    DESCRIPTION: '4등',
     PRIZE: 50000,
   },
   5: {
-    DESCRIPTION: '5개',
+    DESCRIPTION: '3등',
     PRIZE: 1500000,
   },
   5.5: {
-    DESCRIPTION: '5개 + 보너스볼',
+    DESCRIPTION: '2등',
     PRIZE: 30000000,
   },
   6: {
-    DESCRIPTION: '6개',
+    DESCRIPTION: '1등',
     PRIZE: 2000000000,
   },
 };
