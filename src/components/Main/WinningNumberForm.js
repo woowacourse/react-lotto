4aea6b483e182aaaeed9fa9fe33deb41b8943dc8
shinsehantan <shinsehantan@users.noreply.github.com>
2021-04-17 21:02:19 +0900

    refactor: WinningNumberform을 function Component로 migration

--- src/components/Main/WinningNumberForm.js
+++ src/components/Main/WinningNumberForm.js
@@ -1,107 +1,135 @@
-import { Component } from 'react';
-import { ID, MESSAGE } from '../../constants';
+import { MESSAGE } from '../../constants';
 import { isDuplicatedArray } from '../../utils';
+import { useState } from 'react';
 
-export default class WinningNumberForm extends Component {
-  onSubmitWinningNumberForm = (event) => {
+export default function WinningNumberForm(props) {
+  const [winningNumbersInput, setWinningNumbersInput] = useState({});
+  const [bonusNumberInput, setBonusNumberInput] = useState(0);
+
+  const onSubmitWinningNumberForm = (event) => {
     event.preventDefault();
 
-    if (!this.props.lottoCount) {
+    if (!props.lottoCount) {
       alert(MESSAGE.ALERT.LOTTO_NOT_EXIST);
       return;
     }
 
-    const winningNumberInputs =
-      event.target.elements[ID.MAIN.WINNING_NUMBER_FORM.WINNING_NUMBER_FIELDSET].elements;
-    const winningNumbers = Array.from(winningNumberInputs).map(($input) => $input.valueAsNumber);
-    const bonusNumberInput =
-      event.target.elements[ID.MAIN.WINNING_NUMBER_FORM.BONUS_NUMBER_FIELDSET].elements;
-    const [bonusNumber] = Array.from(bonusNumberInput).map(($input) => $input.valueAsNumber);
-
-    if (isDuplicatedArray([...winningNumbers, bonusNumber])) {
+    if (isDuplicatedArray([...Object.values(winningNumbersInput), bonusNumberInput])) {
       alert(MESSAGE.ALERT.DUPLICATED_WINNING_NUMBERS);
       return;
     }
 
-    this.props.setWinningNumbers(winningNumbers);
-    this.props.setBonusNumber(bonusNumber);
-    this.props.openModal();
+    props.openModal();
+    props.setWinningNumbers(Object.values(winningNumbersInput));
+    props.setBonusNumber(bonusNumberInput);
   };
 
-  render() {
-    return (
-      <section className="w-100 mt-5">
-        <form className="w-100 mt-1" onSubmit={this.onSubmitWinningNumberForm}>
-          <label htmlFor="first-winning-number" className="w-100">
-            지난 주 당첨번호 6개와 보너스 넘버 1개를 입력해 주세요.
-          </label>
-          <div className="d-flex mt-3">
-            <fieldset id="winning-number-fieldset" className="d-flex flex-col flex-auto">
-              <legend className="text-center font-bold">당첨번호</legend>
-              <div className="d-flex flex-row justify-space-between">
-                <input
-                  type="number"
-                  min="1"
-                  max="45"
-                  id="first-winning-number"
-                  className="winning-number-input"
-                  required
-                />
-                <input
-                  type="number"
-                  min="1"
-                  max="45"
-                  aria-label="winning-number"
-                  required
-                  className="winning-number-input"
-                />
-                <input
-                  type="number"
-                  min="1"
-                  max="45"
-                  aria-label="winning-number"
-                  required
-                  className="winning-number-input"
-                />
-                <input
-                  type="number"
-                  min="1"
-                  max="45"
-                  aria-label="winning-number"
-                  required
-                  className="winning-number-input"
-                />
-                <input
-                  type="number"
-                  min="1"
-                  max="45"
-                  aria-label="winning-number"
-                  required
-                  className="winning-number-input"
-                />
-                <input
-                  type="number"
-                  min="1"
-                  max="45"
-                  aria-label="winning-number"
-                  required
-                  className="winning-number-input"
-                />
-              </div>
-            </fieldset>
-            <fieldset
-              id="bonus-number-fieldset"
-              className="d-flex flex-col justify-center items-center"
-            >
-              <legend className="text-center font-bold">보너스</legend>
-              <input type="number" className="winning-number-input" min="1" max="45" required />
-            </fieldset>
-          </div>
-          <button className="w-100 basic-button mt-3" type="submit">
-            결과 확인하기
-          </button>
-        </form>
-      </section>
-    );
-  }
+  const onWinningNumberChange = (event) => {
+    setWinningNumbersInput({
+      ...winningNumbersInput,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const onBonusNumberChange = (event) => {
+    setBonusNumberInput(event.target.value);
+  };
+
+  return (
+    <section className="w-100 mt-5">
+      <form className="w-100 mt-1" onSubmit={onSubmitWinningNumberForm}>
+        <label htmlFor="first-winning-number" className="w-100">
+          지난 주 당첨번호 6개와 보너스 넘버 1개를 입력해 주세요.
+        </label>
+        <div className="d-flex mt-3">
+          <fieldset id="winning-number-fieldset" className="d-flex flex-col flex-auto">
+            <legend className="text-center font-bold">당첨번호</legend>
+            <div className="d-flex flex-row justify-space-between">
+              <input
+                type="number"
+                min="1"
+                max="45"
+                id="first-winning-number"
+                className="winning-number-input"
+                name="first-winning-number"
+                value={winningNumbersInput['first-winning-number'] || ''}
+                onChange={onWinningNumberChange}
+                required
+              />
+              <input
+                type="number"
+                min="1"
+                max="45"
+                aria-label="winning-number"
+                className="winning-number-input"
+                name="second-winning-number"
+                value={winningNumbersInput['second-winning-number'] || ''}
+                onChange={onWinningNumberChange}
+                required
+              />
+              <input
+                type="number"
+                min="1"
+                max="45"
+                aria-label="winning-number"
+                name="third-winning-number"
+                value={winningNumbersInput['third-winning-number'] || ''}
+                onChange={onWinningNumberChange}
+                required
+              />
+              <input
+                type="number"
+                min="1"
+                max="45"
+                aria-label="winning-number"
+                name="fourth-winning-number"
+                value={winningNumbersInput['fourth-winning-number'] || ''}
+                onChange={onWinningNumberChange}
+                required
+              />
+              <input
+                type="number"
+                min="1"
+                max="45"
+                aria-label="winning-number"
+                name="fifth-winning-number"
+                value={winningNumbersInput['fifth-winning-number'] || ''}
+                onChange={onWinningNumberChange}
+                required
+              />
+              <input
+                type="number"
+                min="1"
+                max="45"
+                aria-label="winning-number"
+                name="sixth-winning-number"
+                value={winningNumbersInput['sixth-winning-number'] || ''}
+                onChange={onWinningNumberChange}
+                required
+              />
+            </div>
+          </fieldset>
+          <fieldset
+            id="bonus-number-fieldset"
+            className="d-flex flex-col justify-center items-center"
+          >
+            <legend className="text-center font-bold">보너스</legend>
+            <input
+              type="number"
+              className="winning-number-input"
+              name="bonus-number"
+              value={bonusNumberInput || ''}
+              onChange={onBonusNumberChange}
+              min="1"
+              max="45"
+              required
+            />
+          </fieldset>
+        </div>
+        <button className="w-100 basic-button mt-3" type="submit">
+          결과 확인하기
+        </button>
+      </form>
+    </section>
+  );
 }
