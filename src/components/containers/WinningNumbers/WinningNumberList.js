2b9abac854651c59566031b44682e110af1ee7a1
365kim <365listener@gmail.com>
2021-04-18 21:38:12 +0900

    refactor: 불필요한 클래스 삭제

--- src/components/containers/WinningNumbers/WinningNumberList.js
+++ src/components/containers/WinningNumbers/WinningNumberList.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { TwoDigitBall } from '../../shared';
+import { LottoBall } from '../../shared';
 
 export default class WinningNumberList extends Component {
   render() {
@@ -9,34 +9,13 @@ export default class WinningNumberList extends Component {
       <>
         <section className="draw-number-section">
           {winningNumbers.map((v) => (
-            <LottoBall key={v} number={v} />
+            <LottoBall key={v} targetNumber={v} />
           ))}
           <span className="plus-sign">+</span>
           <span className="bonus-number-title">보너스번호</span>
-          <LottoBall key={bonusNumber} number={bonusNumber} />
+          <LottoBall key={bonusNumber} targetNumber={bonusNumber} />
         </section>
       </>
     );
-  }
-}
-
-class LottoBall extends Component {
-  render() {
-    const { number } = this.props;
-    let ballColorClassName;
-
-    if (number < 10) {
-      ballColorClassName = 'zeros';
-    } else if (number < 20) {
-      ballColorClassName = 'tens';
-    } else if (number < 30) {
-      ballColorClassName = 'twenties';
-    } else if (number < 40) {
-      ballColorClassName = 'thirties';
-    } else {
-      ballColorClassName = 'forties';
-    }
-
-    return <TwoDigitBall className={ballColorClassName} number={number} />;
   }
 }
