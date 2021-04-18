e817296432bd2cfe13cc6c0708f8b3720994851a
365kim <60066472+365kim@users.noreply.github.com>
2021-04-19 00:33:29 +0900

    refactor: 버튼의 필수 props 외에는 스프레드로 전달
    Co-authored-by: Jbee <ljyhanll@gmail.com>

--- src/components/shared/PlainButton/index.js
+++ src/components/shared/PlainButton/index.js
@@ -3,10 +3,10 @@ import './style.css';
 
 export default class PlainButton extends Component {
   render() {
-    const { className, onClick, text } = this.props;
+    const { className, text, ...props } = this.props;
 
     return (
-      <button type="button" className={`PlainButton ${className}`} onClick={onClick}>
+      <button type="button" className={`PlainButton ${className}`} {...props}>
         {text}
       </button>
     );
