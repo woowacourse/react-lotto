61923b0657ef7b4df969926b4eba4543749e7ac6
2sooy <1401660@naver.com>
2021-04-19 01:02:46 +0900

    style: 토글 버튼 스타일 추가 및 로또 상세 보기 스크롤 적용
    Co-authored-by: EUNHYUN CHO <devhyun637@users.noreply.github.com>

--- src/components/lottoPurchaseResult/LottoDetailList.js
+++ src/components/lottoPurchaseResult/LottoDetailList.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import LottoDetail from './LottoDetail';
+import styled from 'styled-components';
+
+const DetailListUl = styled.ul`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1.2rem;
+  padding: 0.6rem;
+  max-height: 200px;
+  overflow-y: auto;
+  border: 0.5px solid #e9e2e2;
+  border-radius: 5px;
+`;
 
 class LottoDetailList extends Component {
   render() {
     const { lottos } = this.props;
 
     return (
-      <ul
-        style={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap' }}
-      >
+      <DetailListUl>
         {lottos.map((lotto, idx) => (
           <LottoDetail key={idx} lotto={lotto} />
         ))}
-      </ul>
+      </DetailListUl>
     );
   }
 }
