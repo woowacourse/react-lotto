e763c9fb85ebc6b5dca05336a03185a87f56ce92
yungo1846 <yungo1468@naver.com>
2021-04-18 20:13:05 +0900

    feat: 모달의 dimmed 영역 클릭시 모달이 닫히는 기능

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 export default class Modal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.modalClose = this.modalClose.bind(this);
+  }
+
+  modalClose(e) {
+    if (e.currentTarget === e.target) {
+      this.props.closeModal();
+    }
+  }
+
   render() {
     return (
-      <div className="modal open">
+      <div className="modal open" onClick={this.modalClose}>
         <div className="modal-inner p-10">
-          <button className="modal-close">
-            <svg viewBox="0 0 40 40" onClick={this.props.closeModal}>
+          <button type="button" className="modal-close" onClick={this.props.closeModal}>
+            <svg viewBox="0 0 40 40">
               <path className="close-x" d="M 10,10 L 30,30 M 30,10 L 10,30" />
             </svg>
           </button>
