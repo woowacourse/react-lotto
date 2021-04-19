7d691ba2bd13ce7bd13a1d76eb3a135c08f6513a
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-04-19 23:43:23 +0900

    refactor: 불필요한 memo 삭제

--- src/js/components/purchasedLotto/lottoTicket/LottoTicket.js
+++ src/js/components/purchasedLotto/lottoTicket/LottoTicket.js
@@ -1,14 +1,13 @@
-import React, { memo } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './LottoTicket.scss';
 
-const LottoTicket = memo((props) => (
+const LottoTicket = (props) => (
   <div className="LottoTicket">
     <span className="lotto-ticket-emoji">🎟️</span>
     <p className={props.isShowNumber ? '' : 'd-none'}>{props.lottoNumbers.join(', ')}</p>
   </div>
-));
-
+);
 LottoTicket.propTypes = {
   isShowNumber: PropTypes.bool.isRequired,
   lottoNumbers: PropTypes.array.isRequired,
