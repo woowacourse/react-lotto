a48ea084f5b8a5e98316b39176ee763c2fddc22d
zigsong <wldms5764@gmail.com>
2021-04-19 11:08:27 +0900

    feat: migrate App.js into functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Root, Container, Title } from './style';
 import PriceInput from './components/PriceInput';
 import LottosContainer from './components/LottosContainer';
@@ -7,93 +7,67 @@ import ResultModal from './components/ResultModal';
 import AnnounceTimer from './components/AnnounceTimer';
 import Lotto from './Lotto';
 
-const initialState = {
-  price: 0,
-  lottos: [],
-  winningNumbers: {
-    mainNumbers: [],
-    bonusNumber: null,
-  },
-  isResultModalOpen: false,
-};
+export default function App() {
+  const [price, setPrice] = useState(0);
+  const [lottos, setLottos] = useState([]);
+  const [winningNumbers, setWinningNumbers] = useState({ mainNumbers: [], bonusNumber: null });
+  const [isResultModalOpen, setIsResultModalOpen] = useState(false);
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+  useEffect(() => {
+    createLottos();
+  }, [price]);
 
-    this.state = initialState;
+  const purchaseLottos = (price) => {
+    setPrice(price);
+  };
 
-    this.handleUpdatePrice = this.handleUpdatePrice.bind(this);
-    this.purchaseLottos = this.purchaseLottos.bind(this);
-    this.openResultModal = this.openResultModal.bind(this);
-    this.closeResultModal = this.closeResultModal.bind(this);
-    this.resetGame = this.resetGame.bind(this);
-  }
-
-  handleUpdatePrice(event) {
-    const price = event.target.value;
-    this.setState({ price });
-  }
-
-  purchaseLottos(price) {
-    this.setState({ price }, this.createLottos);
-  }
-
-  createLottos() {
-    const count = this.state.price / Lotto.PRICE_UNIT;
+  const createLottos = () => {
+    const count = price / Lotto.PRICE_UNIT;
     const lottos = Array.from({ length: count }, () => new Lotto(Lotto.generateLottoNumbers()));
 
-    this.setState({ lottos });
-  }
+    console.log(lottos);
+    setLottos(lottos);
+  };
 
-  openResultModal(winningNumbers) {
-    this.setState({
-      winningNumbers,
-      isResultModalOpen: true,
-    });
-  }
+  const openResultModal = (winningNumbers) => {
+    setWinningNumbers(winningNumbers);
+    setIsResultModalOpen(true);
+  };
 
-  closeResultModal() {
-    this.setState({
-      isResultModalOpen: false,
-    });
-  }
+  const closeResultModal = () => {
+    setIsResultModalOpen(false);
+  };
 
-  resetGame() {
-    this.setState(initialState);
-  }
+  const resetGame = () => {
+    setPrice(0);
+    setLottos([]);
+    setWinningNumbers({ mainNumbers: [], bonusNumber: null });
+    setIsResultModalOpen(false);
+  };
 
-  render() {
-    const isLottoCreated = this.state.lottos.length > 0;
+  const isLottosCreated = lottos.length > 0;
 
-    return (
-      <Root>
-        <Container>
-          <Title>🎰 개미 로또</Title>
-          {isLottoCreated ? <AnnounceTimer /> : null}
-          <PriceInput
-            isDisabled={isLottoCreated}
-            onPurchaseLottos={this.purchaseLottos}
-            onUpdatePrice={this.handleUpdatePrice}
-          />
-          {isLottoCreated > 0 ? (
-            <>
-              <LottosContainer lottos={this.state.lottos} />
-              <WinningNumbersContainer winningNumbers={this.state.winningNumbers} onShowResult={this.openResultModal} />
-            </>
-          ) : null}
-        </Container>
-        <ResultModal
-          isOpen={this.state.isResultModalOpen}
-          price={this.state.price}
-          lottos={this.state.lottos}
-          winningNumbers={this.state.winningNumbers}
-          onResetGame={this.resetGame}
-          onCloseModal={this.closeResultModal}
-        />
-      </Root>
-    );
-  }
+  return (
+    <Root>
+      <Container>
+        <Title>🎰 개미 로또</Title>
+        {isLottosCreated ? <AnnounceTimer /> : null}
+        <PriceInput isDisabled={isLottosCreated} onPurchaseLottos={purchaseLottos} />
+        {isLottosCreated ? (
+          <>
+            <LottosContainer lottos={lottos} />
+            <WinningNumbersContainer winningNumbers={winningNumbers} onShowResult={openResultModal} />
+          </>
+        ) : null}
+      </Container>
+      <ResultModal
+        isOpen={isResultModalOpen}
+        price={price}
+        lottos={lottos}
+        winningNumbers={winningNumbers}
+        onResetGame={resetGame}
+        onCloseModal={closeResultModal}
+      />
+    </Root>
+  );
 }
-
-export default App;
