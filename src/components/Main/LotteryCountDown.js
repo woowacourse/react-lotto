411c78b0d4d6016eeff4abdf71da740f6bd9fad0
shinsehantan <shinsehantan@users.noreply.github.com>
2021-04-19 14:34:59 +0900

    refactor: LotteryCountDown을 function Component로 migration

--- src/components/Main/LotteryCountDown.js
+++ src/components/Main/LotteryCountDown.js
@@ -1,38 +1,66 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { toFormattedTimeString, getAnnouncementDate } from '../../utils/lottoUtils';
 
-export default class LotteryCountDown extends Component {
-  state = {
-    remainTime: 0,
-  };
+export default function LotteryCountDown(props) {
+  const [remainTime, setRemainTime] = useState(0);
 
-  componentDidMount = () => {
-    this.tick();
-    this.intervalId = setInterval(this.tick, 1000);
-  };
+  useEffect(() => {
+    tick();
+    const intervalId = setInterval(tick, 1000);
 
-  componentWillUnmount = () => {
-    clearInterval(this.intervalId);
-  };
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
-  tick = () => {
+  const tick = () => {
     const currentTime = new Date();
-    const gap = this.props.announcementDate - currentTime;
+    const gap = props.announcementDate - currentTime;
 
     if (gap < 1000) {
-      this.props.setAnnouncementDate(getAnnouncementDate());
+      props.setAnnouncementDate(getAnnouncementDate());
     }
 
-    this.setState({ remainTime: gap });
+    setRemainTime(gap);
   };
 
-  render() {
-    return (
-      <div className="mt-5 text-center">
-        <h3 className="m-1">🎉 당첨 번호 발표 시간 🎉</h3>
-        <p className="m-1">{this.props.announcementDate.toLocaleString('ko-KR')}</p>
-        <p className="mt-1">남은 시간 : {toFormattedTimeString(this.state.remainTime)}</p>
-      </div>
-    );
-  }
+  console.log(remainTime);
+
+  return (
+    <div className="mt-5 text-center">
+      <h3 className="m-1">🎉 당첨 번호 발표 시간 🎉</h3>
+      <p className="m-1">{props.announcementDate.toLocaleString('ko-KR')}</p>
+      <p className="mt-1">남은 시간 : {toFormattedTimeString(remainTime)}</p>
+    </div>
+  );
 }
+
+// export default class LotteryCountDown extends Component {
+//   state = {
+//     remainTime: 0,
+//   };
+
+//   const componentDidMount = () => {
+//     tick();
+//     intervalId = setInterval(tick, 1000);
+//   };
+
+//   const componentWillUnmount = () => {
+//     clearInterval(intervalId);
+//   };
+
+//   const tick = () => {
+//     const currentTime = new Date();
+//     const gap = props.announcementDate - currentTime;
+
+//     if (gap < 1000) {
+//       props.setAnnouncementDate(getAnnouncementDate());
+//     }
+
+//     setState({ remainTime: gap });
+//   };
+
+//   render() {
+
+//   }
+// }
