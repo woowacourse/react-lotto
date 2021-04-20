7e3a2ff8886088f908f5de6f224630c01322f795
sunhpark42 <sunhpark42@gmail.com>
2021-04-20 11:29:06 +0900

    refactor: htmlFor, id 삭제 및 input form 중복 해소
    참조 : https://github.com/woowacourse/react-lotto/pull/12#discussion_r615891236

--- src/components/Main/PurchaseForm.js
+++ src/components/Main/PurchaseForm.js
@@ -32,27 +32,26 @@ export default class PurchaseForm extends Component {
     return (
       <section className="mt-5">
         <form className="w-100" onSubmit={this.onSubmitPurchaseForm}>
-          <label htmlFor="money-input" className="w-100">
+          <label className="w-100">
             구입할 금액을 입력해주세요. (단위: 원)
+            <div className="d-flex justify-space-between items-center mt-1">
+              <input
+                className="money-input flex-auto mr-3"
+                type="number"
+                placeholder="구입 금액"
+                min="1000"
+                required
+                disabled={this.state.isActive ? false : true}
+              />
+              <button
+                type="submit"
+                className="basic-button"
+                disabled={this.state.isActive ? false : true}
+              >
+                확인
+              </button>
+            </div>
           </label>
-          <div className="d-flex justify-space-between items-center mt-1">
-            <input
-              id="money-input"
-              className="money-input flex-auto mr-3"
-              type="number"
-              placeholder="구입 금액"
-              min="1000"
-              required
-              disabled={this.state.isActive ? false : true}
-            />
-            <button
-              type="submit"
-              className="basic-button"
-              disabled={this.state.isActive ? false : true}
-            >
-              확인
-            </button>
-          </div>
         </form>
       </section>
     );
--- src/components/Main/WinningNumberForm.js
+++ src/components/Main/WinningNumberForm.js
@@ -32,71 +32,34 @@ export default class WinningNumberForm extends Component {
     return (
       <section className="w-100 mt-5">
         <form className="w-100 mt-1" onSubmit={this.onSubmitWinningNumberForm}>
-          <label htmlFor="first-winning-number" className="w-100">
+          <label className="w-100">
             지난 주 당첨번호 6개와 보너스 넘버 1개를 입력해 주세요.
+            <div className="d-flex mt-3">
+              <fieldset id="winning-number-fieldset" className="d-flex flex-col flex-auto">
+                <legend className="text-center font-bold">당첨번호</legend>
+                <div className="d-flex flex-row justify-space-between">
+                  <input type="number" min="1" max="45" className="winning-number-input" required />
+                  {Array.from({ length: 6 }).map(() => (
+                    <input
+                      type="number"
+                      min="1"
+                      max="45"
+                      aria-label="winning-number"
+                      required
+                      className="winning-number-input"
+                    />
+                  ))}
+                </div>
+              </fieldset>
+              <fieldset
+                id="bonus-number-fieldset"
+                className="d-flex flex-col justify-center items-center"
+              >
+                <legend className="text-center font-bold">보너스</legend>
+                <input type="number" className="winning-number-input" min="1" max="45" required />
+              </fieldset>
+            </div>
           </label>
-          <div className="d-flex mt-3">
-            <fieldset id="winning-number-fieldset" className="d-flex flex-col flex-auto">
-              <legend className="text-center font-bold">당첨번호</legend>
-              <div className="d-flex flex-row justify-space-between">
-                <input
-                  type="number"
-                  min="1"
-                  max="45"
-                  id="first-winning-number"
-                  className="winning-number-input"
-                  required
-                />
-                <input
-                  type="number"
-                  min="1"
-                  max="45"
-                  aria-label="winning-number"
-                  required
-                  className="winning-number-input"
-                />
-                <input
-                  type="number"
-                  min="1"
-                  max="45"
-                  aria-label="winning-number"
-                  required
-                  className="winning-number-input"
-                />
-                <input
-                  type="number"
-                  min="1"
-                  max="45"
-                  aria-label="winning-number"
-                  required
-                  className="winning-number-input"
-                />
-                <input
-                  type="number"
-                  min="1"
-                  max="45"
-                  aria-label="winning-number"
-                  required
-                  className="winning-number-input"
-                />
-                <input
-                  type="number"
-                  min="1"
-                  max="45"
-                  aria-label="winning-number"
-                  required
-                  className="winning-number-input"
-                />
-              </div>
-            </fieldset>
-            <fieldset
-              id="bonus-number-fieldset"
-              className="d-flex flex-col justify-center items-center"
-            >
-              <legend className="text-center font-bold">보너스</legend>
-              <input type="number" className="winning-number-input" min="1" max="45" required />
-            </fieldset>
-          </div>
           <button className="w-100 basic-button mt-3" type="submit">
             결과 확인하기
           </button>
