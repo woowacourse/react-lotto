20d4546a22c610f2729f61b149b63717110098f1
Chayan <hchayan196@gmail.com>
2021-04-20 23:32:59 +0900

    refactor: this.props.remainTime destructuring 방식으로 리팩토링

--- src/components/RemainedTime/RemainedTime.tsx
+++ src/components/RemainedTime/RemainedTime.tsx
@@ -10,14 +10,15 @@ export default class RemainedTime extends Component<Props> {
   }
 
   render() {
+    const { remainTime } = this.props;
     return (
       <RemainedTimeWrapper>
         🎯 로또 당첨 발표까지 🎯
         <p className="remain-time-text">
-          <span>{this.props.remainTime?.getDate()}</span> 일&nbsp;
-          <span>{this.props.remainTime?.getHours()}</span> 시간&nbsp;
-          <span>{this.props.remainTime?.getMinutes()}</span> 분&nbsp;
-          <span>{this.props.remainTime?.getSeconds()}</span> 초
+          <span>{remainTime?.getDate()}</span> 일&nbsp;
+          <span>{remainTime?.getHours()}</span> 시간&nbsp;
+          <span>{remainTime?.getMinutes()}</span> 분&nbsp;
+          <span>{remainTime?.getSeconds()}</span> 초
         </p>
         남았습니다.
       </RemainedTimeWrapper>
