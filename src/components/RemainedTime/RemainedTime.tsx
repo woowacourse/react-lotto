e641d65eeeeb7c3398ffc1c81705ddc7a663c063
Chayan <hchayan196@gmail.com>
2021-04-20 23:34:40 +0900

    refactor: 불필요한 예외처리용 type 제거

--- src/components/RemainedTime/RemainedTime.tsx
+++ src/components/RemainedTime/RemainedTime.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { RemainedTimeWrapper } from './RemainedTime.styles';
 
 type Props = {
-  remainTime: Date | null;
+  remainTime: Date;
 };
 export default class RemainedTime extends Component<Props> {
   constructor(props: Props) {
@@ -15,10 +15,10 @@ export default class RemainedTime extends Component<Props> {
       <RemainedTimeWrapper>
         🎯 로또 당첨 발표까지 🎯
         <p className="remain-time-text">
-          <span>{remainTime?.getDate()}</span> 일&nbsp;
-          <span>{remainTime?.getHours()}</span> 시간&nbsp;
-          <span>{remainTime?.getMinutes()}</span> 분&nbsp;
-          <span>{remainTime?.getSeconds()}</span> 초
+          <span>{remainTime.getDate()}</span> 일&nbsp;
+          <span>{remainTime.getHours()}</span> 시간&nbsp;
+          <span>{remainTime.getMinutes()}</span> 분&nbsp;
+          <span>{remainTime.getSeconds()}</span> 초
         </p>
         남았습니다.
       </RemainedTimeWrapper>
