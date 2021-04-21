1c40dc3f5f2639d7115403733958e0464763977a
365kim <365listener@gmail.com>
2021-04-22 01:43:18 +0900

    feat: <Xbutton /> 함수 컴포넌트로 변경

--- src/components/shared/XButton/index.js
+++ src/components/shared/XButton/index.js
@@ -1,16 +1,16 @@
-import { Component } from 'react';
+import PropTypes from 'prop-types';
 import './style.css';
 
-export default class XButton extends Component {
-  render() {
-    const { onClick } = this.props;
-
-    return (
-      <button type="button" className="XButton" onClick={onClick}>
-        <svg className="XButton__svg" viewBox="0 0 40 40">
-          <path className="XButton__svg_path" d="M 10,10 L 30,30 M 30,10 L 10,30" />
-        </svg>
-      </button>
-    );
-  }
+export default function XButton({ onClick }) {
+  return (
+    <button type="button" className="XButton" onClick={onClick}>
+      <svg className="XButton__svg" viewBox="0 0 40 40">
+        <path className="XButton__svg_path" d="M 10,10 L 30,30 M 30,10 L 10,30" />
+      </svg>
+    </button>
+  );
 }
+
+XButton.propTypes = {
+  onClick: PropTypes.func,
+};
