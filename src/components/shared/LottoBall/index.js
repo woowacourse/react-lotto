ed6973621a062101945b716bff2fab30820a3a24
365kim <365listener@gmail.com>
2021-04-22 01:29:24 +0900

    feat: <LottoBall /> 함수 컴포넌트로 변경

--- src/components/shared/LottoBall/index.js
+++ src/components/shared/LottoBall/index.js
@@ -1,22 +1,24 @@
-import { Component } from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import Ball from '../Ball';
 import styles from './style.css';
 
 const cx = classNames.bind(styles);
 
-export default class LottoBall extends Component {
-  render() {
-    const { targetNumber: num, winningNumbers } = this.props;
-    const lottoBallClass = cx({
-      'LottoBall--zeros': num < 10,
-      'LottoBall--tens': num >= 10 && num < 20,
-      'LottoBall--twenties': num >= 20 && num < 30,
-      'LottoBall--thirties': num >= 30 && num < 40,
-      'LottoBall--forties': num >= 40,
-      'LottoBall--not_matched': winningNumbers && !winningNumbers.includes(num),
-    });
+export default function LottoBall({ targetNumber: num, winningNumbers }) {
+  const lottoBallClass = cx({
+    'LottoBall--zeros': num < 10,
+    'LottoBall--tens': num >= 10 && num < 20,
+    'LottoBall--twenties': num >= 20 && num < 30,
+    'LottoBall--thirties': num >= 30 && num < 40,
+    'LottoBall--forties': num >= 40,
+    'LottoBall--not_matched': winningNumbers && !winningNumbers.includes(num),
+  });
 
-    return <Ball className={lottoBallClass}>{num.toString().padStart(2, '0')}</Ball>;
-  }
+  return <Ball className={lottoBallClass}>{num.toString().padStart(2, '0')}</Ball>;
 }
+
+LottoBall.propTypes = {
+  targetNumber: PropTypes.number.isRequired,
+  winningNumbers: PropTypes.array,
+};
