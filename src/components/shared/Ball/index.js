f881855430f97d265ffe3baf9015f8d2781368d9
365kim <365listener@gmail.com>
2021-04-22 01:20:36 +0900

    feat: <Ball /> 함수 컴포넌트로 변경

--- src/components/shared/Ball/index.js
+++ src/components/shared/Ball/index.js
@@ -1,16 +1,21 @@
-import { Component } from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import styles from './style.css';
 
 const cx = classNames.bind(styles);
-export default class Ball extends Component {
-  render() {
-    const { className, children } = this.props;
-    const BallClass = cx({
-      Ball: true,
-      [`${className}`]: true,
-    });
 
-    return <span className={BallClass}>{children}</span>;
-  }
+export default function Ball({ className, children }) {
+  const ballClassnames = cx('Ball', `${className}`);
+
+  return <span className={ballClassnames}>{children}</span>;
 }
+
+Ball.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.string,
+};
+
+Ball.defaultProps = {
+  className: '',
+  children: '',
+};
