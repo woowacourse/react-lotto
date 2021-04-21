33998cd1db1cb1917011b3119ab1fc9c0c4bcd82
365kim <365listener@gmail.com>
2021-04-22 01:42:14 +0900

    feat: <ToggleButton /> 함수 컴포넌트로 변경

--- src/components/shared/ToggleButton/index.js
+++ src/components/shared/ToggleButton/index.js
@@ -1,18 +1,18 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-import { Component } from 'react';
+import PropTypes from 'prop-types';
 import './style.css';
 
-export default class ToggleButton extends Component {
-  render() {
-    const { onChange, children } = this.props;
-
-    return (
-      <div className="ToggleButton">
-        <label className="ToggleButton__label">
-          <input type="checkbox" className="ToggleButton__input" onChange={onChange} />
-          <span className="ToggleButton__text">{children}</span>
-        </label>
-      </div>
-    );
-  }
+export default function ToggleButton({ onChange, children }) {
+  return (
+    <div className="ToggleButton">
+      <label className="ToggleButton__label">
+        <input type="checkbox" className="ToggleButton__input" onChange={onChange} />
+        <span className="ToggleButton__text">{children}</span>
+      </label>
+    </div>
+  );
 }
+
+ToggleButton.propTypes = {
+  onChange: PropTypes.func,
+  children: PropTypes.any,
+};
