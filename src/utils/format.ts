9e03854dba461eece030acb7c1b080e2ce60aa40
Chayan <hchayan196@gmail.com>
2021-04-21 18:26:53 +0900

    refactor: 빠른 리턴 타입 맞춰줘서 일관된 반환 타입 사용

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,5 +1,5 @@
 export const getKRMoneyString = (number: number) => {
-  if (Number.isNaN(number)) return;
+  if (Number.isNaN(number)) throw new Error('NaN');
 
   return number.toLocaleString('ko-KR');
 };
