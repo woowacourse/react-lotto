26f613b028d2d21f4d7574d8b5d3494c1cee6a18
365kim <365listener@gmail.com>
2021-04-22 01:40:02 +0900

    feat: <Title /> 함수 컴포넌트로 변경

--- src/components/shared/Title/index.js
+++ src/components/shared/Title/index.js
@@ -1,29 +1,32 @@
-import { Component } from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import styles from './style.css';
 
 const cx = classNames.bind(styles);
 
-export default class Title extends Component {
-  render() {
-    const { as, size, children } = this.props;
-    const TitleClass = cx({ Title, [`Title--${size}`]: true });
+export default function Title({ as, size, children }) {
+  const TitleClass = cx('Title', { [`Title--${size}`]: Boolean(size) });
 
-    switch (as) {
-      case 'h1':
-        return <h1 className={TitleClass}>{children}</h1>;
-      case 'h2':
-        return <h2 className={TitleClass}>{children}</h2>;
-      case 'h3':
-        return <h3 className={TitleClass}>{children}</h3>;
-      case 'h4':
-        return <h4 className={TitleClass}>{children}</h4>;
-      case 'h5':
-        return <h5 className={TitleClass}>{children}</h5>;
-      case 'h6':
-        return <h6 className={TitleClass}>{children}</h6>;
-      default:
-        return <h2 className={TitleClass}>{children}</h2>;
-    }
+  switch (as) {
+    case 'h1':
+      return <h1 className={TitleClass}>{children}</h1>;
+    case 'h2':
+      return <h2 className={TitleClass}>{children}</h2>;
+    case 'h3':
+      return <h3 className={TitleClass}>{children}</h3>;
+    case 'h4':
+      return <h4 className={TitleClass}>{children}</h4>;
+    case 'h5':
+      return <h5 className={TitleClass}>{children}</h5>;
+    case 'h6':
+      return <h6 className={TitleClass}>{children}</h6>;
+    default:
+      return <h2 className={TitleClass}>{children}</h2>;
   }
 }
+
+Title.propTypes = {
+  as: PropTypes.string,
+  size: PropTypes.string,
+  children: PropTypes.any,
+};
