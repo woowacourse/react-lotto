f89580bdde85fdca73bd609582cac35e44f74bfb
Chayan <hchayan196@gmail.com>
2021-04-24 17:39:13 +0900

    refactor: remainTime 이용한 조건부 렌더링 구현

--- src/components/RemainedTime/RemainedTime.tsx
+++ src/components/RemainedTime/RemainedTime.tsx
@@ -17,21 +17,25 @@ const RemainedTime = () => {
   useEffect(() => {
     handleRemainedTime();
     return () => {
-      clearTimeout(remainTimer);
+      remainTimer && clearTimeout(remainTimer);
     };
   }, []);
 
   return (
-    <RemainedTimeWrapper>
-      🎯 로또 당첨 발표까지 🎯
-      <p className="remain-time-text">
-        <span>{remainTime?.getDate()}</span> 일&nbsp;
-        <span>{remainTime?.getHours()}</span> 시간&nbsp;
-        <span>{remainTime?.getMinutes()}</span> 분&nbsp;
-        <span>{remainTime?.getSeconds()}</span> 초
-      </p>
-      남았습니다.
-    </RemainedTimeWrapper>
+    <>
+      {remainTime && (
+        <RemainedTimeWrapper>
+          🎯 로또 당첨 발표까지 🎯
+          <p className="remain-time-text">
+            <span>{remainTime.getDate()}</span> 일&nbsp;
+            <span>{remainTime.getHours()}</span> 시간&nbsp;
+            <span>{remainTime.getMinutes()}</span> 분&nbsp;
+            <span>{remainTime.getSeconds()}</span> 초
+          </p>
+          남았습니다.
+        </RemainedTimeWrapper>
+      )}
+    </>
   );
 };
 
