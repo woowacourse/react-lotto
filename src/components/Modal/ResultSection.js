ea326e013dfe424c0398b87166a21af17b7d9839
sunhpark42 <sunhpark42@gmail.com>
2021-04-29 16:57:49 +0900

    refactor: ResultSection을 함수컴포넌트로 변경

--- src/components/Modal/ResultSection.js
+++ src/components/Modal/ResultSection.js
@@ -1,28 +1,26 @@
-import { Component } from 'react';
 import { LOTTO_VALUE } from '../../constants';
-export default class ResultSection extends Component {
-  rankKey = ['FIRST', 'SECOND', 'THIRD', 'FOURTH', 'FIFTH'];
 
-  render() {
-    return (
-      <table className="mt-3">
-        <tbody>
-          <tr>
-            <th className="p-2">일치 갯수</th>
-            <th className="p-2">당첨금</th>
-            <th className="p-2">당첨 갯수</th>
+const rankKey = ['FIRST', 'SECOND', 'THIRD', 'FOURTH', 'FIFTH'];
+
+export default function ResultSection({ winningCounts }) {
+  return (
+    <table className="mt-3">
+      <tbody>
+        <tr>
+          <th className="p-2">일치 갯수</th>
+          <th className="p-2">당첨금</th>
+          <th className="p-2">당첨 갯수</th>
+        </tr>
+        {rankKey.map((key) => (
+          <tr className="text-center" key={key}>
+            <td className="p-2">{LOTTO_VALUE.WINNING_CONDITION_TEXT[key]}</td>
+            <td className="p-2">{LOTTO_VALUE.WINNING_PRIZE[key].toLocaleString()}</td>
+            <td className="p-2">
+              <span>{winningCounts[LOTTO_VALUE.RANK[key]]}</span>개
+            </td>
           </tr>
-          {this.rankKey.map((key) => (
-            <tr className="text-center" key={key}>
-              <td className="p-2">{LOTTO_VALUE.WINNING_CONDITION_TEXT[key]}</td>
-              <td className="p-2">{LOTTO_VALUE.WINNING_PRIZE[key].toLocaleString()}</td>
-              <td className="p-2">
-                <span>{this.props.winningCounts[LOTTO_VALUE.RANK[key]]}</span>개
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
+        ))}
+      </tbody>
+    </table>
+  );
 }
