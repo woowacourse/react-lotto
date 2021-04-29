bf3b3f3d4a5f4c78dfe363155f8fd66f327d7f8b
sunhpark42 <sunhpark42@gmail.com>
2021-04-29 16:41:21 +0900

    refactor: LottoItem 을 함수 컴포넌트로 변경

--- src/components/Main/LottoItem.js
+++ src/components/Main/LottoItem.js
@@ -1,63 +1,51 @@
-import { Component } from 'react';
+import { useEffect, useRef } from 'react';
 import { LOTTO_VALUE } from '../../constants';
 import { getRandomNumberArray } from '../../utils';
 
-export default class LottoItem extends Component {
-  state = {
-    numbers: getRandomNumberArray(
-      LOTTO_VALUE.MIN_NUMBER,
-      LOTTO_VALUE.MAX_NUMBER,
-      LOTTO_VALUE.NUMBER_COUNT
-    ),
-  };
+const getMatchedCount = (array1, array2) => {
+  return array1.length + array2.length - new Set([...array1, ...array2]).size;
+};
 
-  componentDidUpdate(prevProps) {
-    if (this.props.isModalOpened && this.props.isModalOpened !== prevProps.isModalOpened) {
-      this.increaseWinningCounts();
-    }
+const getWinningCount = (matchedCount, hasBonusNumber) => {
+  switch (matchedCount) {
+    case LOTTO_VALUE.MATCHED_COUNT.FIRST:
+      return LOTTO_VALUE.RANK.FIRST;
+    case LOTTO_VALUE.MATCHED_COUNT.THIRD:
+      return hasBonusNumber ? LOTTO_VALUE.RANK.SECOND : LOTTO_VALUE.RANK.THIRD;
+    case LOTTO_VALUE.MATCHED_COUNT.FOURTH:
+      return LOTTO_VALUE.RANK.FOURTH;
+    case LOTTO_VALUE.MATCHED_COUNT.FIFTH:
+      return LOTTO_VALUE.RANK.FIFTH;
+    default:
+      return;
   }
+};
 
-  // 일치하는 개수를 구하는 연산식
-  getMatchedCount = () => {
-    return (
-      this.props.winningNumbers.length +
-      this.state.numbers.length -
-      new Set([...this.props.winningNumbers, ...this.state.numbers]).size
-    );
-  };
+export default function LottoItem({
+  bonusNumber,
+  winningNumbers,
+  isModalOpened,
+  isToggled,
+  increaseWinningCounts,
+}) {
+  const numbers = useRef(
+    getRandomNumberArray(LOTTO_VALUE.MIN_NUMBER, LOTTO_VALUE.MAX_NUMBER, LOTTO_VALUE.NUMBER_COUNT)
+  );
 
-  getWinningCount = (matchedCount) => {
-    const hasBonusNumber = this.state.numbers.includes(this.props.bonusNumber);
+  useEffect(() => {
+    if (isModalOpened) {
+      const hasBonusNumber = numbers.current.includes(bonusNumber);
+      const matchedCount = getMatchedCount(winningNumbers, numbers.current);
+      const winningCount = getWinningCount(matchedCount, hasBonusNumber);
 
-    switch (matchedCount) {
-      case LOTTO_VALUE.MATCHED_COUNT.FIRST:
-        return LOTTO_VALUE.RANK.FIRST;
-      case LOTTO_VALUE.MATCHED_COUNT.THIRD:
-        return hasBonusNumber ? LOTTO_VALUE.RANK.SECOND : LOTTO_VALUE.RANK.THIRD;
-      case LOTTO_VALUE.MATCHED_COUNT.FOURTH:
-        return LOTTO_VALUE.RANK.FOURTH;
-      case LOTTO_VALUE.MATCHED_COUNT.FIFTH:
-        return LOTTO_VALUE.RANK.FIFTH;
-      default:
-        return;
+      winningCount && increaseWinningCounts(winningCount);
     }
-  };
+  }, [isModalOpened]);
 
-  increaseWinningCounts = () => {
-    const matchedCount = this.getMatchedCount();
-    const winningCount = this.getWinningCount(matchedCount);
-
-    winningCount && this.props.increaseWinningCounts(winningCount);
-  };
-
-  render() {
-    const isToggled = this.props.isToggled;
-
-    return (
-      <li className={`lotto-item ${isToggled ? 'toggle' : ''}`}>
-        <span className="lotto-icon">🎟</span>
-        {isToggled && <span>{[...this.state.numbers].join(', ')}</span>}
-      </li>
-    );
-  }
+  return (
+    <li className={`lotto-item ${isToggled ? 'toggle' : ''}`}>
+      <span className="lotto-icon">🎟</span>
+      {isToggled && <span>{[...numbers.current].join(', ')}</span>}
+    </li>
+  );
 }
