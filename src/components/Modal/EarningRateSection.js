c02985dd3234dbe1258bb49fd34d4b6eb53e9479
sunhpark42 <sunhpark42@gmail.com>
2021-04-29 17:06:40 +0900

    refactor: EarningRateSection를 함수컴포넌트로 변경

--- src/components/Modal/EarningRateSection.js
+++ src/components/Modal/EarningRateSection.js
@@ -1,26 +1,23 @@
-import { Component } from 'react';
 import { PRIZE_BY_RANK } from '../../constants';
-export default class EarningRateSection extends Component {
-  getEarnedMoney = () => {
-    return Object.keys(this.props.winningCounts).reduce(
-      (sum, key) => sum + this.props.winningCounts[key] * PRIZE_BY_RANK[key],
+
+export default function EarningRateSection({ winningCounts, paidMoney }) {
+  const getEarnedMoney = () => {
+    return Object.keys(winningCounts).reduce(
+      (sum, key) => sum + winningCounts[key] * PRIZE_BY_RANK[key],
       0
     );
   };
 
-  calculateEarningRate = () => {
-    const earnedMoney = this.getEarnedMoney();
-    const paidMoney = this.props.paidMoney;
+  const calculateEarningRate = () => {
+    const earnedMoney = getEarnedMoney();
 
     return Math.floor(((earnedMoney - paidMoney) / paidMoney) * 100);
   };
 
-  render() {
-    return (
-      <div className="mt-5 text-center">
-        🎉🎉 당신의 총 수익률은 <span className="font-bold">{this.calculateEarningRate()}</span>
-        %입니다. 🎉🎉
-      </div>
-    );
-  }
+  return (
+    <div className="mt-5 text-center">
+      🎉🎉 당신의 총 수익률은 <span className="font-bold">{calculateEarningRate()}</span>
+      %입니다. 🎉🎉
+    </div>
+  );
 }
