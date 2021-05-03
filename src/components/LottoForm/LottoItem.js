99e2d4aa31db369f8618259257c7fe4c06ad0d7e
sunhpark42 <sunhpark42@gmail.com>
2021-05-04 01:16:43 +0900

    refactor: defualt 값이 없는 삼항연산자 식을 && 을 이용한 식으로 변경

--- src/components/LottoForm/LottoItem.js
+++ src/components/LottoForm/LottoItem.js
@@ -1,6 +1,6 @@
 export default function LottoItem({ isToggled, numbers }) {
   return (
-    <li className={`lotto-item ${isToggled ? 'toggle' : ''}`}>
+    <li className={`lotto-item ${isToggled && 'toggle'}`}>
       <span className="lotto-icon">🎟</span>
       {isToggled && <span>{[...numbers].join(', ')}</span>}
     </li>
