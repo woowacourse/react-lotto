47103f8ae1adf7a132e1462dd752c557c4808a1c
sunhpark42 <sunhpark42@gmail.com>
2021-05-04 01:03:05 +0900

    refactor: 불필요한 함수 선언 삭제

--- src/components/LottoResult/EarningRateSection.js
+++ src/components/LottoResult/EarningRateSection.js
@@ -1,22 +1,15 @@
 import { PRIZE_BY_RANK } from '../../constants';
 
 export default function EarningRateSection({ winningCounts, paidMoney }) {
-  const getEarnedMoney = () => {
-    return Object.keys(winningCounts).reduce(
-      (sum, key) => sum + winningCounts[key] * PRIZE_BY_RANK[key],
-      0
-    );
-  };
-
-  const calculateEarningRate = () => {
-    const earnedMoney = getEarnedMoney();
-
-    return Math.floor(((earnedMoney - paidMoney) / paidMoney) * 100);
-  };
+  const earnedMoney = Object.keys(winningCounts).reduce(
+    (sum, key) => sum + winningCounts[key] * PRIZE_BY_RANK[key],
+    0
+  );
+  const earningRate = Math.floor(((earnedMoney - paidMoney) / paidMoney) * 100);
 
   return (
     <div className="mt-5 text-center">
-      🎉🎉 당신의 총 수익률은 <span className="font-bold">{calculateEarningRate()}</span>
+      🎉🎉 당신의 총 수익률은 <span className="font-bold">{earningRate}</span>
       %입니다. 🎉🎉
     </div>
   );
