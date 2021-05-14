26ba38f95737d6930ea7b9d924bb0cd7363cba13
365kim <365listener@gmail.com>
2021-05-14 21:09:15 +0900

    refactor: useToggleButton 커스텀 훅 추가

--- src/components/containers/UserLotto/index.js
+++ src/components/containers/UserLotto/index.js
@@ -1,23 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import { Lotto } from './Lotto';
-import { ToggleButton } from '../../shared';
+import { useToggleButton } from '../../../hooks';
 import styles from './style.css';
 
 const cx = classNames.bind(styles);
-const initialState = { isToggled: false };
 
 export const UserLotto = (props) => {
   const { lottoBundle } = props;
-  const [isToggled, setIsToggled] = useState(initialState.isToggled);
-  const onChangeToggleButton = (e) => setIsToggled(e.target.checked);
+  const { HookedToggleButton: ToggleButton, isToggled } = useToggleButton(false);
 
   return (
     <div className="UserLotto">
-      <ToggleButton isToggled={isToggled} onChange={onChangeToggleButton}>
-        번호보기
-      </ToggleButton>
+      <ToggleButton>번호보기</ToggleButton>
       <p>
         총 <span className="UserLotto__number">{lottoBundle.length}</span>개 구매하였습니다.
       </p>
---
+++ src/hooks/index.js
@@ -0,0 +1,3 @@
+import { useToggleButton } from './useToggleButton';
+
+export { useToggleButton };
