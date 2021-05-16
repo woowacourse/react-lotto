21602374b0ad325581a9acc2507cd44eb0272001
권세진 <0307kwon@gmail.com>
2021-05-16 17:58:01 +0900

    feat: dimmed 영역을 눌러도 모달이 닫히도록 수정

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,10 +3,25 @@ import React from "react";
 const Modal = (props) => {
   const { isModalOpen, closeModal, children } = props;
 
+  const onCloseButtonClick = ({ target, currentTarget }) => {
+    if (target === currentTarget) {
+      closeModal();
+      return;
+    }
+
+    if (target.closest(".modal-close")) {
+      closeModal();
+      return;
+    }
+  };
+
   return (
-    <div className={`modal ${isModalOpen ? "open" : ""}`}>
+    <div
+      className={`modal ${isModalOpen ? "open" : ""}`}
+      onClick={onCloseButtonClick}
+    >
       <div className="modal-inner p-10">
-        <div className="modal-close" onClick={closeModal}>
+        <div className="modal-close">
           <svg viewBox="0 0 40 40">
             <path className="close-x" d="M 10,10 L 30,30 M 30,10 L 10,30" />
           </svg>
