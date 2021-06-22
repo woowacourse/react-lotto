d0242f710e8e01a2e91fc095d4fe900cc0f39cc6
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-06-22 17:40:33 +0900

    refactor: 불필요한 화살표 함수 -> 함수로 수정

--- src/components/@commons/Modal/ModalCloseButton/ModalCloseButton.js
+++ src/components/@commons/Modal/ModalCloseButton/ModalCloseButton.js
@@ -7,7 +7,7 @@ export const ModalCloseButton = props => {
   const { onClick } = props;
 
   return (
-    <Button type="button" aria-label="close-button" onClick={() => onClick()}>
+    <Button type="button" aria-label="close-button" onClick={onClick}>
       <svg aria-hidden="true" viewBox="0 0 40 40">
         <path d="M 10, 10 L 30,30 M 30,10 L 10,30" />
       </svg>
