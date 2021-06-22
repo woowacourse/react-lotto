7eaffd1fbb6f3d63d00bf86c131ac08fdbad8a4f
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-06-22 22:54:01 +0900

    fix: useState(setState)에서 updater 사용
    - useSelectNumbers의 numbers 상태를 setState의 updater를 활용하여 업데이트

--- src/hooks/useSelectNumbers.js
+++ src/hooks/useSelectNumbers.js
@@ -5,20 +5,19 @@ import { LOTTO, MESSAGE } from '../constants';
 export const useSelectNumbers = () => {
   const [numbers, setNumbers] = useState([]);
 
-  const handleSelectNumbers = pickedNumber => {
-    const prevNumbers = numbers;
+  const handleSelectNumbers = pickedNumber =>
+    setNumbers(prevNumbers => {
+      const newNumbers = prevNumbers.find(number => number === pickedNumber)
+        ? prevNumbers.filter(number => number !== pickedNumber)
+        : [...prevNumbers, pickedNumber];
 
-    const newNumbers = numbers.find(number => number === pickedNumber)
-      ? prevNumbers.filter(number => number !== pickedNumber)
-      : [...prevNumbers, pickedNumber];
-
-    if (newNumbers.length === LOTTO.BUNDLE_SIZE + 1) {
-      alert(MESSAGE.EXCEEDED_LOTTO_COUNT);
-      return;
-    }
-
-    setNumbers(newNumbers);
-  };
+      if (newNumbers.length === LOTTO.BUNDLE_SIZE + 1) {
+        alert(MESSAGE.EXCEEDED_LOTTO_COUNT);
+        return prevNumbers;
+      } else {
+        return newNumbers;
+      }
+    });
 
   return [numbers, handleSelectNumbers];
 };
