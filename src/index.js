debe513ff1c7a69cee241f9132301e44e6f7696a
2sooy <1401660@naver.com>
2021-06-28 07:54:51 +0900

    feat: GlobalStyle 적용

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import GlobalStyle from './components/GlobalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
