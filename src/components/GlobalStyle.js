f13edd8fd319b9dff7875a08ef8d86f6a98a959e
2sooy <1401660@naver.com>
2021-06-27 22:38:10 +0900

    feat: GlobalStyle 컴포넌트 추가

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default GlobalStyle;
