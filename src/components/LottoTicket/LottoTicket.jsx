ca7fcbdf933a1ce381b7290d58340a966fd33161
jum0 <wnsah052@naver.com>
2021-06-28 21:32:44 +0900

    feat: image 에러 핸들링 기능

--- src/components/LottoTicket/LottoTicket.jsx
+++ src/components/LottoTicket/LottoTicket.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './LottoTicket.styles.css';
 import lottoImage from '../../images/ticket.png';
+import noImage from '../../images/no-image.png';
 import { LOTTO_NUMBER_SEPARATOR } from '../../constants/message';
 
 const LottoTicket = ({ lotto }) => {
   return (
     <div className="lotto">
-      <img className="lotto-image" src={lottoImage} alt="lotto" />
+      <img className="lotto-image" src={lottoImage} alt="lotto" onError={noImage} />
       <span className="lotto-number">{lotto.map((v) => (v < 10 ? `0${v}` : v)).join(LOTTO_NUMBER_SEPARATOR)}</span>
     </div>
   );
